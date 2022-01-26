0b7cef56270a9e0dfb52aa503758c4f22f62ec46
Bayubotzz <96980508+Bayubotzz@users.noreply.github.com>
2022-01-26 19:14:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['6289525518325'] // Put your number here
+global.mods = ['6289525518325'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -28,11 +28,11 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ShiraoriBOT Multi device'
-global.author = 'Ilman'
-global.email = 'wakabahiiro5@gmail.com'
+global.packname = 'ELAINA-BOT Multi device'
+global.author = 'BAYU OFC'
+global.email = 'Bayubotz888@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© ShiraoriBOT-Md Made by Ilman'
+global.wm = '© ELAINA-BOT-Md Made by BAYU OFC'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
